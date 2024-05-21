04958d960b1196c54646509cd8cba5406438372a
Sanju4456 <sanjunimesha40@gmail.com>
2024-05-21 15:01:26 +1000

    second

--- selenium-week10/src/test/java/sit707/AddTest.java
+++ selenium-week10/src/test/java/sit707/AddTest.java
@@ -5,8 +5,14 @@ import org.junit.Test;
 
 
 public class AddTest{
-	@Test
-	public void testAddValidAB() {
-		Assert.assertEquals(MathUtil.add(1,2),3);
-	}
+	 @Test
+	    public void testAddition() {
+	        // Testing addition of two positive numbers
+	        Assert.assertEquals(MathUtil.add(5, 3), 8);
+	    }
+	 @Test
+	    public void testAddValidAB() {
+	        // This test case intentionally fails by asserting an incorrect result
+	        Assert.assertEquals(MathUtil.add(1, 2), 4);
+	    }
 }
